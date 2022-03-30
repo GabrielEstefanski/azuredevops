efb9f25d0b073996e26708bd20c03ab01d57b8e4
Gabriel Braga Estefanski <gabriel.estefanski@2rpnet.com>
2022-03-29 23:21:49 -0300

    created azure.py and configurations of flake8

---
+++ azure.py
@@ -0,0 +1,41 @@
+import json
+import pandas as pd
+from requests_pkcs12 import get
+
+df = []
+
+
+def get_request(url) -> str:
+    response = get(
+        url,
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": "Basic <token>",
+        },
+        verify=False,
+    )
+
+    return json.loads(response.content.decode("utf-8"))
+
+
+def search(data) -> None:
+    for values in data["value"]:
+        df.append(
+            [
+                values["id"],
+                values["name"],
+                values["url"],
+                values["state"],
+                values["revision"],
+                values["visibility"],
+            ]
+        )
+
+    dataframe = pd.DataFrame(
+        df, columns=["id", "name", "url", "state", "revision", "visibility"]
+    )
+    print(dataframe)
+
+
+data = get_request("https://dev.azure.com/desenv2rp/_apis/projects?")
+resp = search(data)
